dc9e7da045820ff23602acb595fc439f755a9667
LandryLT <59315119+LandryLT@users.noreply.github.com>
2022-02-02 22:31:43 +0100

    save my work

--- WWWJDIC.py
+++ WWWJDIC.py
@@ -1,14 +1,34 @@
+from dataclasses import replace
+from email import message
+from logging import exception
 import re
 from bs4 import BeautifulSoup
 import requests
 import urllib.parse
 import os.path
 
+class NoMoreHits(Exception):
+    def __init__(self, word) -> None:
+        self.message = "No more " + word + " could be found"
+        super().__init__(self.message)
+
+class NoHits(Exception):
+    def __init__(self, word) -> None:
+        self.message = word + " could not be found AT ALL..."
+        super().__init__(self.message)
+
 class WWWJDIC:
-    def __init__(self, word: str, sound_download_dir: str) -> None:
+    def __init__(self, word: str, sound_download_dir: str, rendered_soup= None, excludedIDs=[]) -> None:
         self.word = word
-        self.allsoup = self.renderWWWJDIC()
-        self.bestsoup = self.find_word_definition()
+        
+        # Allready rendered page
+        self.allsoup = rendered_soup
+        if self.allsoup is None:
+            self.allsoup = self.renderWWWJDIC()
+        
+        self.bestsoup = self.find_word_definition(excludedIDs)
+        self.labelID = self.get_ID()
+        self.kana = self.get_kana()
         self.jap_sentence, self.eng_sentence = self.get_sentence()
         self.sound_file = self.get_sound(sound_download_dir)
     
@@ -21,29 +41,43 @@ class WWWJDIC:
         return BeautifulSoup(requests.post(url, request_body).content, 'html.parser')
 
     # Get best guess of dict entry
-    def find_word_definition(self) -> BeautifulSoup:
+    def find_word_definition(self, excludedIDs) -> BeautifulSoup:
         # Get dic entries
         allheadwords_soup = self.allsoup.find("form", id="inp").find_all("div", style="clear: both;")
-       
+
         # Strip headwords
         for e in allheadwords_soup:
+            
+            # Allready done in previous WWWJDIC search
+# MUST TEST !!!!!!!!!!!!!
+            labelID = e.find('input')['id']
+            if labelID in excludedIDs:
+                continue
+# MUST TEST !!!!!!!!!
+
             headword = e.find('label').find('font', size='+1').stripped_strings
             headword_str = ''
             for s in headword:
                 headword_str = headword_str + s           
             
             # Regex just the kanji part
-            headword_str = re.match(r'(?P<hdwrd>^.*)(【.*】|《.*》)', headword_str).group('hdwrd')
-            hits = re.findall(r'[^;\s]+', headword_str)          
+            reg_pattern = r'(?P<hdwrd>^.*)(【.*】|《(?P<kanjiopt>.*)》| )'
+            hdwrd = re.match(reg_pattern, headword_str).group('hdwrd')
+            extra_kanji = re.match(reg_pattern, headword_str).group('kanjiopt')
+            if extra_kanji is not None:
+                hdwrd += '; ' + extra_kanji
+            hits = re.findall(r'[^;\s]+', hdwrd)          
             
             # If matches exactly self.word
             for h in hits:
                 if re.match(re.compile(h), self.word):
                     return e
             
-        # Else raise error
-        print(self.word + " could not be found in Jim Breen's WWWJDIC")
-        raise
+        # Else raise exception
+        if len(excludedIDs) == 0:
+            raise NoHits(self.word)
+        else:
+            raise NoMoreHits(self.word)
 
 
     # Get example sentence and translation
@@ -51,10 +85,6 @@ class WWWJDIC:
         
         br = self.bestsoup.find('br')
         
-        # No sentences...
-        if br is None:
-            return (None, None)
-
         # Possible sentences !
         both_l = ''
         for s in br.next_sibling.contents[2:-2]:
@@ -62,15 +92,17 @@ class WWWJDIC:
         
         # Seperate Japanes and English
         both_l_re = re.match(r'(?P<jap>^.*)(\t)(?P<eng>.*$)', both_l)
+        if both_l_re is None:
+            return (None, None)
         
         return (both_l_re.group('jap'), both_l_re.group('eng'))
 
 
     # Download sound if any
-    def get_sound(self, dir: str):
+    def get_sound(self, dir: str) -> str:
 
         # Find query string 
-        query_string = re.match(r'm\(\'(?P<tophp>.+)\'\)',self.bestsoup.find('script').text).group('tophp')
+        query_string = re.match(r'm\(\'(?P<qstring>.+)\'\)',self.bestsoup.find('script').text).group('qstring')
         
         # No sound
         if query_string is None:
@@ -93,5 +125,12 @@ class WWWJDIC:
         
         return soundfile_path
 
-    def get_kana(self):
-        pass
+
+    # Get kana
+    def get_kana(self) -> str:
+        kana_url = re.match(r'm\(\'kana=(?P<kana>.+)\&kanji=.*\'\)', self.bestsoup.find('script').text).group('kana')
+        return urllib.parse.unquote(kana_url)
+
+    # Get unique LabelID
+    def get_ID(self) -> str:
+        return self.bestsoup.find('input')['id']
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import ssl
+from warnings import catch_warnings
 from KanjiSljfaq import KanjiSljfaq
-from WWWJDIC import WWWJDIC
+from WWWJDIC import WWWJDIC, NoHits, NoMoreHits
 
 # Create unprotected SSL context /!\
 ssl._create_default_https_context = ssl._create_unverified_context
 
 word_1 = '新婦'
 word_2 = 'する'
+word_3 = '滑る'
 # list_kan = []
 # for c in word:
 #     list_kan.append(c)
 # newKan = KanjiSljfaq(list_kan)
-
-word_def = WWWJDIC(word_2, "./.sounds/")
+try:
+    word_def = WWWJDIC(word_3, "./.sounds/")
+except NoHits:
+    raise
+except NoMoreHits as e:
+    print(e)
 
 # Print stuff
 # print(word_def.sound_file)
