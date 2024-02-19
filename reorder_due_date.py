c8addcb708f36182d5993eb2e183c632d843e33f
LandryLT <59315119+LandryLT@users.noreply.github.com>
2024-02-19 20:07:58 +0100

    no print

--- reorder_due_date.py
+++ reorder_due_date.py
@@ -8,7 +8,7 @@ def reorder_due_by_newest(deck_name):
     new_cards_rev_due_sorted = list(map(anki_col.getCard, anki_col.find_cards('deck:"'+deck_name+'" is:new', "c.due desc")))
 
     for i in  range(len(new_cards_date_sorted)):
-        print(i, new_cards_date_sorted[i].due, new_cards_rev_due_sorted[i].due)
+
         setattr(new_cards_date_sorted[i], "due", new_cards_rev_due_sorted[i].due)
         new_cards_date_sorted[i].flush()
 
